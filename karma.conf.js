a74748235e18d5bd97d7b24295f08fef803733e2
Gabriel Vance <ilovenarwhales@gmail.com>
2025-01-08 10:48:57 -0600

    adddd

--- karma.conf.js
+++ karma.conf.js
@@ -20,14 +20,64 @@ module.exports = function (config) {
       dir: path.join(__dirname, "./coverage/my-angular-app"),
       subdir: ".",
       reporters: [{ type: "html" }, { type: "text-summary" }],
-    },
+      },
+      customLaunchers: {
+        ChromeHeadlessNoSandbox: {
+          base: 'ChromeHeadless',
+          flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
+        }
+      },
+      browsers: ['ChromeHeadlessNoSandbox'],
     reporters: ["progress", "coverage"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ["ChromeHeadless"],
+    // browsers: ["ChromeHeadless"],
     singleRun: true, // Set to true for CI/CD or headless mode
     restartOnFileChange: true,
   });
 };
+
+
+// maybe this ?
+
+// services:
+//   - name: docker
+//     config:
+//       options: --shm-size=2g
+
+
+// npm install--save - dev karma - firefox - launcher
+// browsers: ['FirefoxHeadless'],
+
+
+// or this
+
+// plugins: ['karma-parallel'],
+// parallelOptions: {
+//   executors: 4, // Number of processes
+//   shardStrategy: 'round-robin',
+// }
+
+
+
+// karma start --grep=someTestGroup
+
+
+
+// or this
+
+
+// browsers: ['ChromeHeadless'],
+// captureTimeout: 120000, // Increase capture timeout
+// browserDisconnectTimeout: 10000, // Time to wait for a reconnect
+// browserDisconnectTolerance: 3,  // Number of disconnects allowed
+// browserNoActivityTimeout: 120000 // Timeout for no activity
+
+
+// or this
+
+
+
+// ng test some-product --include "/src/features/(apple|cherry|lemon)/**/*.spec.ts"
